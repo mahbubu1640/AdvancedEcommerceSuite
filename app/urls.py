fb3e325632915addc27ac57e2504cd1b4d34ef4b
Mahbubur Rahman <104581346+mahbubu1640@users.noreply.github.com>
2024-03-11 13:47:48 +0530

    updated product detail views making to dynamic for all product

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.ProductView.as_view(),name='home'),
     path('add_product/', views.ProductCreateView.as_view(), name='add_product'),
-    path('product-detail/<int:pk>', views.product_detail, name='product-detail'),
+    path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
     path('profile/', views.profile, name='profile'),
--- app/views.py
+++ app/views.py
@@ -33,9 +33,16 @@ class ProductCreateView(CreateView):
 # def product_detail(request):
 #  return render(request, 'app/productdetail.html')
 
-def product_detail(request,pk):
-    product=Product.objects.get(pk=pk)
-    return render(request,'app/productdetail.html',{'product':product})
+# def product_detail(request,pk):
+#     product=Product.objects.get(pk=pk)
+#     return render(request,'app/productdetail.html',{'product':product})
+
+
+
+class ProductDetailView(View):
+    def get(self,request,pk):
+        product = Product.objects.get(pk=pk)
+        return render(request,'app/productdetail.html',{'product':product})
 
 def add_to_cart(request):
  return render(request, 'app/addtocart.html')
